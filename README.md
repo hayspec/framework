33e25967b726695b12adfad5275f71a13c65aaed
xpeper <xpepermint@gmail.com>
2018-06-15 23:45:27 +0200

    Initialize project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hayspec Framework
+
+> Elegant testing framework.
+
+## Requirements
+
+* NodeJS v9.0.0+
+* Linux or Mac OS X.
+
+## Development
+
+```
+$ npx lerna run build
+$ npx lerna publish --force-publish
+```
---
+++ packages/hayspec-core/src/tests/index.test.ts
@@ -0,0 +1,10 @@
+// import { assert } from 'chai';
+// import * as utils from '..';
+
+// describe('index', function () {
+
+//   it('exposes isDirectory method', async function () {
+//     assert.equal(!!utils.isDirectory, true);
+//   });
+
+// });
