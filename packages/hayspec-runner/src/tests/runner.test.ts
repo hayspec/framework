84df14f0153957ad913bc3dc20a7a9be44de5604
xpeper <xpepermint@gmail.com>
2018-06-20 18:33:25 +0200

    Fix test type

--- packages/hayspec-runner/src/tests/runner.test.ts
+++ packages/hayspec-runner/src/tests/runner.test.ts
@@ -1,7 +1,7 @@
 import test from 'ava';
 import { Runner } from '..';
 
-test('method `load` loads spec files based on pattern', async (t) => {
+test('method `require` loads spec files based on pattern', async (t) => {
   const runner = new Runner();
   await runner.require('./src/tests/assets/**/*.hay.ts');
   t.is(runner.results.length, 2);
