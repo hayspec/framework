daf5774b307488aa18b33221a4b364ee24a75f6c
Kristijan Sedlak <xpepermint@gmail.com>
2018-10-04 17:39:54 +0200

    Fix CLI, bump version

--- packages/hayspec-cli/src/lib/env.ts
+++ packages/hayspec-cli/src/lib/env.ts
@@ -1,7 +1,21 @@
 import * as pt from 'path';
 
+/**
+ * Returns package.json data.
+ */
+export function getPackage() {
+  try {
+    return require(pt.join(process.cwd(), 'package.json')) || {};
+  } catch (e) {
+    return {};
+  }
+}
+
+/**
+ * Returns Hayspec options.
+ */
 export function getConfig(argv?: any) {
-  const defaults = require(pt.join(process.cwd(), 'package.json'))['hayspec'] || {};
+  const defaults = getPackage()['hayspec'] || {};
   const custom = argv || {};
   return {
     name: custom['name'] || defaults['name'] || '',
