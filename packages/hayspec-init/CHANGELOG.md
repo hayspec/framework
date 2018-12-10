afeaae26181724577d7e031aaf01c03f08a2f430
Kristijan Sedlak <xpepermint@gmail.com>
2018-12-10 22:11:17 +0100

    Update tests, bump version

--- packages/hayspec-init/CHANGELOG.md
+++ packages/hayspec-init/CHANGELOG.md
@@ -1,6 +1,11 @@
 # Change Log - @hayspec/init
 
-This log was last generated on Mon, 10 Dec 2018 20:16:37 GMT and should not be manually modified.
+This log was last generated on Mon, 10 Dec 2018 21:06:44 GMT and should not be manually modified.
+
+## 0.8.2
+Mon, 10 Dec 2018 21:06:44 GMT
+
+*Version update only*
 
 ## 0.8.1
 Mon, 10 Dec 2018 20:16:37 GMT
--- packages/hayspec-spec/src/tests/methods/request.test.ts
+++ packages/hayspec-spec/src/tests/methods/request.test.ts
@@ -1,8 +1,13 @@
 import test from 'ava';
 import { request } from '../../methods/request';
 
-test('continues with timeout', async (t) => {
+test('performs a http request', async (t) => {
   const res = await request({ url: 'https://jsonplaceholder.typicode.com/todos/1' });
   t.is(res.status, 200);
   t.is(res.data.userId, 1);
+});
+
+test('handles errors', async (t) => {
+  const res = await request({ url: 'https://jsonplaceholder.typicode.com/foo', method: 'post' });
+  t.is(res.status, 404);
 });
